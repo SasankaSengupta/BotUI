10c60cd94a179b20b6d6a0445e145bbbb9128c1d
SasankaSengupta <33123504+SasankaSengupta@users.noreply.github.com>
2018-04-11 17:22:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var builder = require('botbuilder');
 var restify = require('restify');
+var botbuilder_azure = require("botbuilder-azure");
 var inMemoryStorage = new builder.MemoryBotStorage();
 // var connector = new builder.ConsoleConnector().listen();
 // var bot = new builder.UniversalBot(connector, function (session) {
@@ -20,30 +21,163 @@ var connector = new builder.ChatConnector({
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
-
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, [
+var tableName = 'botTestdata';
+var storageName = "phonebotmarisionia46a";
+var storageKey = "PHA9uaJRytfGpIvF8LWgCf7dF2fiMdg9RrdDgI1dlf28XPvXlbBuXrWHkoGWmGmOEMA+3WIgj0Ensl8PF4wbcw==";
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, storageName,storageKey);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+var bot = new builder.UniversalBot(connector);
+bot.dialog('/', [
     function (session) {
-        session.beginDialog('userProfile',session.userData.profile);
-    },
-    
-    function(session,results){
-        session.userData.profile = results.response; // save user profile
-        session.send(`Hello ${session.userData.profile.name}! You work for ${session.userData.profile.company}!`);
+        session.beginDialog('showPromo');
     }
-]).set('storage',inMemoryStorage);
+]);
+
+
+bot.set('storage', tableStorage);
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+               // bot.beginDialog(message.address, 'showPromo');
+               var txt = "Hello user";
+               var reply = new builder.Message()
+                .address(message.address)
+                .text(txt);
+                bot.send(reply);
+            }
+            
+        });
+    }
+    
+    
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+                .address(message.address)
+                .text("Hello %s... Thanks for adding me.", name || 'there');
+        bot.send(reply);
+        bot.beginDialog('showPromo');
+    }
+});
+
+bot.dialog('normalPromo', function (session) {
+    var msg = new builder.Message(session);
+    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+    msg.attachments([
+        new builder.HeroCard(session)
+            .title("Promotions")
+            .text("Hi, I saw you like my offer ':-)' do you want to proceed  with the payment?")
+            
+            .images([builder.CardImage.create(session, 'https://www.apkmirror.com/wp-content/uploads/2018/02/5a923158e6749.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "yes", "Yes"),
+                builder.CardAction.imBack(session, "no", "No")
+            ])
+    ]);
+    session.send(msg).endDialog();
+});
+
+// Add dialog to handle 'Buy' button click
+bot.dialog('yesDialogButtonClick', [
+    function (session, args, next) {
+        
+            // Invalid product
+            session.send("Great! I am starting the process right now, give me a sec").endDialog();
+        
+    }
+]).triggerAction({ matches: /(yes|add)/i });
+
+bot.dialog('noDialogButtonClick', [
+    function (session, args, next) {
+        
+            // Invalid product
+            session.send("No problem. Would you like to see etc etc…").endDialog();
+        
+    }
+]).triggerAction({ matches: /(no|add)/i });
+
+// Add dialog to return list of shirts available
+bot.dialog('showPromo', function (session) {
+    var msg = new builder.Message(session);
+    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+    msg.attachments([
+        new builder.HeroCard(session)
+            .title("Promotions")
+            .text("Hi, I saw you like my offer  😊 do you want to proceed  with the payment?")
+            
+            .images([builder.CardImage.create(session, 'https://www.apkmirror.com/wp-content/uploads/2018/02/5a923158e6749.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "yes", "Yes"),
+                builder.CardAction.imBack(session, "no", "No")
+            ])
+    ]);
+    session.send(msg).endDialog();
+});
+
+// Add dialog to handle 'Buy' button click
+bot.dialog('yesButtonClick', [
+    function (session, args, next) {
+        
+            // Invalid product
+            session.send("Great! I am starting the process right now, give me a sec").endDialog();
+        
+    }
+]).triggerAction({ matches: /(yes|add)/i });
+
+bot.dialog('noButtonClick', [
+    function (session, args, next) {
+        
+            // Invalid product
+            session.send("No problem. Would you like to see etc etc…").endDialog();
+        
+    }
+]).triggerAction({ matches: /(no|add)/i });
+
+bot.dialog('showOffers',[
+    function(session){
+        var msg = new builder.Message(session);
+        msg.attachmentLayout(builder.AttachmentLayout.carousel);
+         msg.attachments([
+             new builder.HeroCard(session)
+            .title("Promotions")
+            
+            .text("Hi, I saw you like my offer  😊 do you want to proceed  with the payment?")
+            .images([builder.CardImage.create(session, 'https://www.apkmirror.com/wp-content/uploads/2018/02/5a923158e6749.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "YES", "YES"),
+                builder.CardAction.imBack(session, "NO", "NO")
+            ])
+         ]);
+         session.send(msg).endDialog();
+    }
+]);
+
+
+
 
 bot.dialog('userProfile',[
     function(session,args,next){
+        session.sendTyping();
         session.dialogData.profile = args || {}; //set profile or create the object.
         
         if(!session.dialogData.profile.name){
-            session.send("Let us know about you!!");
-            builder.Prompts.text(session,'What is your name?');
+            setTimeout(function () {
+                session.send("Let us know about you!!");
+                builder.Prompts.text(session,'What is your name?');
+            },3000);
+            
         }
         else
         {
-            session.send("As per our prior interactions we know you!!");
+            setTimeout(function () {
+                session.send("As per our prior interactions we know you!!");
+            },3000);
+            
             next();//skip if we have user name 
         }
     },
@@ -68,3 +202,4 @@ bot.dialog('userProfile',[
         session.endDialogWithResult({response:session.dialogData.profile});
     }
 ]);
+
